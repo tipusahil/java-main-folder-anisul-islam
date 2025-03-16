368c7123a3d816eb81fc56c37fc422b5c14da117
tipusahil <tipusahil.ctg@gmail.com>
2025-03-16 21:41:17 +0600

    java & js e usr theke input newar binnota

---
+++ user_input_in_java/InputFromUser.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class InputFromUser {
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+         
+        int insideUserValue ;
+
+        System.out.println("hello boss give us your input");
+        insideUserValue = userInput.nextInt();
+
+        // int numbers = 1234;
+        // System.out.println("number holo = " + numbers);
+    }
+
+}
---
+++ user_input_in_java/javaScript.js
@@ -0,0 +1,4 @@
+var userInput  = prompt("hello boss give me your input value : ");
+userInput =  parseInt(userInput)
+console.log(userInput)
+// js browser e load hoi ortat prompt nile browser theke seta deka jabe.
